cab6b5972648fdbd409542505bc8c09514851b84
Dikasteria <rjhey@hotmail.com>
2019-07-29 10:58:58 +0100

    users and devies migrations updates

--- migrations/20190729104906_create_table_users.js
+++ migrations/20190729104906_create_table_users.js
@@ -1,8 +1,10 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("users", usersTable => {
+    usersTable.increments("id").primary();
+    usersTable.string("name").notNullable();
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTable("users");
 };
--- migrations/20190729104927_create_table_devices.js
+++ migrations/20190729104927_create_table_devices.js
@@ -1,8 +1,14 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("devices", devicesTable => {
+    devicesTable.increments("id").primary();
+    devicesTable
+      .integer("user_id")
+      .references("users.id")
+      .notNullable();
+    devicesTable.string("push_key").notNullable();
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTable("devicesTable");
 };
