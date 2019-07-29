b70bf2286d5be426c2a8685f289702c5f832642c
Dikasteria <rjhey@hotmail.com>
2019-07-29 12:44:58 +0100

    seeds setup complete

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,1 +1,24 @@
 const { users, devices, events, meds, quiz } = require("../index");
+
+exports.seed = function(knex, Promise) {
+  return knex.migrate
+    .rollback()
+    .then(() => {
+      return knex.migrate.latest();
+    })
+    .then(() => {
+      return knex("users").insert(users);
+    })
+    .then(() => {
+      const devicesInsertions = knex("devices").insert(devices);
+      const eventsInsertions = knex("events").insert(events);
+      const medsInsertions = knex("meds").insert(meds);
+      const quizInsertions = knex("quiz").insert(quiz);
+      return Promise.all([
+        devicesInsertions,
+        eventsInsertions,
+        medsInsertions,
+        quizInsertions
+      ]);
+    });
+};
