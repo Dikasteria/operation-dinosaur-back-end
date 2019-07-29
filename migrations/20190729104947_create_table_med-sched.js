4463ee5a75b0f0d4ba05c59246df9be026cc7257
Dikasteria <rjhey@hotmail.com>
2019-07-29 11:36:49 +0100

    meds amendments

--- migrations/20190729104947_create_table_med-sched.js
+++ migrations/20190729104947_create_table_med-sched.js
@@ -1,8 +1,24 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("meds", medsTable => {
+    medsTable.increments("id").primary();
+    medsTable
+      .integer("user_id")
+      .references("users.id")
+      .notNullable();
+    medsTable.string("type").notNullable();
+    medsTable.timestamp("due").notNullable();
+    medsTable
+      .boolean("taken")
+      .notNullable()
+      .defaultTo(false);
+    medsTable.timestamp("taken_at");
+    medsTable
+      .integer("status")
+      .defaultTo(0)
+      .notNullable();
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTable("meds");
 };
