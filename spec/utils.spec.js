dff27620f8d1f316bfae5ddd5d0ab8c98985c665
Dikasteria <rjhey@hotmail.com>
2019-07-29 14:35:38 +0100

    test utils functions

---
+++ spec/utils.spec.js
@@ -0,0 +1,92 @@
+const { expect } = require("chai");
+const { formatDate, makeRefObj, formatComments } = require("../db/utils/utils");
+
+describe("formatDate", () => {
+  it("returns a new array", () => {
+    const input = [];
+    const expected = [];
+    const actual = formatDate(input, "created_at");
+    expect(actual).to.eql(expected);
+    expect(actual).not.to.equal(expected);
+  });
+  it("formats one object", () => {
+    const input = [
+      {
+        title: "Living in the shadow of a great man",
+        created_at: 1542284514171
+      }
+    ];
+    const expected = [
+      {
+        title: "Living in the shadow of a great man",
+        created_at: new Date(1542284514171)
+      }
+    ];
+    const actual = formatDate(input, "created_at");
+    expect(actual).to.eql(expected);
+  });
+  it("formats all objects", () => {
+    const input = [
+      {
+        title: "Living in the shadow of a great man",
+        created_at: 1542284514171
+      },
+      {
+        title: "Sony Vaio; or, The Laptop",
+        created_at: 1416140514171
+      },
+      {
+        title: "Eight pug gifs that remind me of mitch",
+        created_at: 1289996514171
+      }
+    ];
+    const expected = [
+      {
+        title: "Living in the shadow of a great man",
+        created_at: new Date(1542284514171)
+      },
+      {
+        title: "Sony Vaio; or, The Laptop",
+        created_at: new Date(1416140514171)
+      },
+      {
+        title: "Eight pug gifs that remind me of mitch",
+        created_at: new Date(1289996514171)
+      }
+    ];
+    const actual = formatDate(input, "created_at");
+    expect(actual).to.eql(expected);
+  });
+  it("formats all given key name", () => {
+    const input = [
+      {
+        title: "Living in the shadow of a great man",
+        timestamp: 1542284514171
+      },
+      {
+        title: "Sony Vaio; or, The Laptop",
+        timestamp: 1416140514171
+      },
+      {
+        title: "Eight pug gifs that remind me of mitch",
+        timestamp: 1289996514171
+      }
+    ];
+    const expected = [
+      {
+        title: "Living in the shadow of a great man",
+        timestamp: new Date(1542284514171)
+      },
+      {
+        title: "Sony Vaio; or, The Laptop",
+        timestamp: new Date(1416140514171)
+      },
+      {
+        title: "Eight pug gifs that remind me of mitch",
+        timestamp: new Date(1289996514171)
+      }
+    ];
+    const actual = formatDate(input, "timestamp");
+    expect(actual).to.eql(expected);
+  });
+});
