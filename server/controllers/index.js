1bd40829a7af3a30c1ed74e7c6e352f86a3e2549
Alice-Claire <aliceclaireh@hotmail.co.uk>
2019-07-30 17:11:03 +0100

    add index routes for med and events controllers

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,4 +1,23 @@
-const { addUser } = require("./usersController");
-const { fetchDevices, addDevice } = require("./devicesControllers");
+const { addUser } = require('./usersController');
+const { fetchDevices, addDevice } = require('./devicesController');
+const { fetchMeds, addMed, updateMed, removeMed } = require('./medsController');
+const {
+  fetchEvents,
+  addEvent,
+  updateEvent,
+  removeEvent
+} = require('./eventsController');
 
-module.exports = { addUser, fetchDevices, addDevice };
+module.exports = {
+  addUser,
+  fetchDevices,
+  addDevice,
+  fetchMeds,
+  addMed,
+  updateMed,
+  removeMed,
+  fetchEvents,
+  addEvent,
+  updateEvent,
+  removeEvent
+};
