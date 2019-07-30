a76467bbec933d611da09c9eaacb29aee9ebeca4
Alice-Claire <aliceclaireh@hotmail.co.uk>
2019-07-30 17:11:36 +0100

    add events router to api router

--- server/routers/apiRouter.js
+++ server/routers/apiRouter.js
@@ -1,12 +1,14 @@
-const express = require("express");
+const express = require('express');
 const apiRouter = express.Router();
-const { usersRouter } = require("./usersRouter");
-const { devicesRouter } = require("./devicesRouter");
-const { medsRouter } = require("./medsRouter");
+const { usersRouter } = require('./usersRouter');
+const { devicesRouter } = require('./devicesRouter');
+const { medsRouter } = require('./medsRouter');
+const { eventsRouter } = require('./eventsRouter');
 
 // apiRouter.route("/").all(errHandle405);
-apiRouter.use("/users", usersRouter);
-apiRouter.use("/devices", devicesRouter);
-apiRouter.use("/meds", medsRouter);
+apiRouter.use('/users', usersRouter);
+apiRouter.use('/devices', devicesRouter);
+apiRouter.use('/meds', medsRouter);
+apiRouter.use('/events', eventsRouter);
 
 module.exports = { apiRouter };
