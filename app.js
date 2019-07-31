9952af5e04b89e1ca8cc21650775e4f211aad770
Alice-Claire <aliceclaireh@hotmail.co.uk>
2019-07-31 16:23:18 +0100

    add handling for custom errors and server errors

--- app.js
+++ app.js
@@ -2,12 +2,18 @@ const { apiRouter } = require('./server/routers/apiRouter');
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const { handlePsqlErrors } = require('./errors');
+const {
+  handleCustomErrors,
+  handlePsqlErrors,
+  handleServerErrors
+} = require('./errors');
 
 app.use(express.json());
 app.use(cors());
 app.use('/api', apiRouter);
 
+app.use(handleCustomErrors);
 app.use(handlePsqlErrors);
+app.use(handleServerErrors);
 
 module.exports = { app };
