bea7368e689a4f410b9c49ae8a9020fee444647f
Alice-Claire <aliceclaireh@hotmail.co.uk>
2019-07-31 10:01:31 +0100

    refactor models index

--- server/models/index.js
+++ server/models/index.js
@@ -1,44 +1,13 @@
-const { postUser } = require('./userModel');
-const { getDevices, postDevice } = require('./devicesModel');
-const { getMeds, postMed, patchMed, deleteMed } = require('./medsModel');
-const {
-  getEvents,
-  postEvent,
-  patchEvent,
-  deleteEvent
-} = require('./eventsModel');
-const { getQuiz, postQuiz, patchQuiz, deleteQuiz } = require('./quizModel');
-
-module.exports = {
-  postUser,
-  getDevices,
-  postDevice,
-  getMeds,
-  postMed,
-  patchMed,
-  deleteMed,
-  getEvents,
-  postEvent,
-  patchEvent,
-  deleteEvent,
-  getQuiz,
-  postQuiz,
-  patchQuiz,
-  deleteQuiz
-};
-
-```suggestion
-const usermodels = require('./userModel');
+const userModels = require('./userModel');
 const deviceModels = require('./devicesModel');
 const medsModels = require('./medsModel');
 const eventModels = require('./eventsModel');
 const quizModels = require('./quizModel');
 
 module.exports = {
-  ...usermodels,
+  ...userModels,
   ...deviceModels,
   ...medsModels,
   ...eventModels,
   ...quizModels
 };
-```
