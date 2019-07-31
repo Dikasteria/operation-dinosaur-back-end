838556493712d85d0e90b074632542d6fde445fe
Alice-Claire <aliceclaireh@hotmail.co.uk>
2019-07-31 12:06:55 +0100

    add fetch users controller

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -1,7 +1,13 @@
-const { postUser } = require("../models/");
+const { postUser, getUser } = require('../models/');
 
 exports.addUser = (req, res, next) => {
   postUser({ ...req.body }).then(user => {
     res.status(201).send({ user });
+  });
+};
+
+exports.fetchUser = (req, res, next) => {
+  getUser({ ...req.params }).then(user => {
+    res.status(200).send({ user });
   });
 };
