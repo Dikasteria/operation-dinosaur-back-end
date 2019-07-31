9c7e1cf8e80bbedf902a364f258b233476144d65
Alice-Claire <aliceclaireh@hotmail.co.uk>
2019-07-31 08:01:10 +0100

    add quiz router to api router

--- server/routers/apiRouter.js
+++ server/routers/apiRouter.js
@@ -4,11 +4,13 @@ const { usersRouter } = require('./usersRouter');
 const { devicesRouter } = require('./devicesRouter');
 const { medsRouter } = require('./medsRouter');
 const { eventsRouter } = require('./eventsRouter');
+const { quizRouter } = require('./quizRouter');
 
 // apiRouter.route("/").all(errHandle405);
 apiRouter.use('/users', usersRouter);
 apiRouter.use('/devices', devicesRouter);
 apiRouter.use('/meds', medsRouter);
 apiRouter.use('/events', eventsRouter);
+apiRouter.use('/quiz', quizRouter);
 
 module.exports = { apiRouter };
