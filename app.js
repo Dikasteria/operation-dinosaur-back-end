d5210fe183a980b508698e8a60865ea6535974d2
Alice-Claire <aliceclaireh@hotmail.co.uk>
2019-07-31 15:45:18 +0100

    add handler for psql errors to app

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-const { apiRouter } = require("./server/routers/apiRouter");
-const express = require("express");
+const { apiRouter } = require('./server/routers/apiRouter');
+const express = require('express');
 const app = express();
-const cors = require("cors");
+const cors = require('cors');
+const { handlePsqlErrors } = require('./errors');
 
 app.use(express.json());
 app.use(cors());
-app.use("/api", apiRouter);
+app.use('/api', apiRouter);
+
+app.use(handlePsqlErrors);
 
 module.exports = { app };
