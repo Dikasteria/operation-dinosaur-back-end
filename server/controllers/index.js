11e9f2db2f4b0182b4c8f1012a5779b921ecc96e
Alice-Claire <aliceclaireh@hotmail.co.uk>
2019-07-31 10:01:41 +0100

    refactor controllers index

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,33 +1,13 @@
-const { addUser } = require('./usersController');
-const { fetchDevices, addDevice } = require('./devicesController');
-const { fetchMeds, addMed, updateMed, removeMed } = require('./medsController');
-const {
-  fetchEvents,
-  addEvent,
-  updateEvent,
-  removeEvent
-} = require('./eventsController');
-const {
-  fetchQuiz,
-  addQuiz,
-  updateQuiz,
-  removeQuiz
-} = require('./quizController');
+const userControllers = require('./usersController');
+const deviceControllers = require('./devicesController');
+const medsControllers = require('./medsController');
+const eventControllers = require('./eventsController');
+const quizControllers = require('./quizController');
 
 module.exports = {
-  addUser,
-  fetchDevices,
-  addDevice,
-  fetchMeds,
-  addMed,
-  updateMed,
-  removeMed,
-  fetchEvents,
-  addEvent,
-  updateEvent,
-  removeEvent,
-  fetchQuiz,
-  addQuiz,
-  updateQuiz,
-  removeQuiz
+  ...userControllers,
+  ...deviceControllers,
+  ...medsControllers,
+  ...eventControllers,
+  ...quizControllers
 };
