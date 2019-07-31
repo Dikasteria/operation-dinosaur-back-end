dff972b7f77faeb597318b5631b507d2b840b933
Alice-Claire <aliceclaireh@hotmail.co.uk>
2019-07-31 12:54:57 +0100

    add 404 handling logic for get devices

--- server/controllers/devicesController.js
+++ server/controllers/devicesController.js
@@ -1,9 +1,18 @@
 const { getDevices, postDevice } = require('../models');
 
 exports.fetchDevices = (req, res, next) => {
-  getDevices({ ...req.params }).then(devices => {
-    res.status(200).send({ devices });
-  });
+  getDevices({ ...req.params })
+    .then(devices => {
+      if (devices.length < 1) {
+        res.status(404).send({ msg: 'No devices found' });
+      } else if (devices.length === 1) {
+        const device = devices[0];
+        res.status(200).send({ device });
+      } else {
+        res.status(200).send({ devices });
+      }
+    })
+    .catch(next);
 };
 
 exports.addDevice = (req, res, next) => {
--- server/models/devicesModel.js
+++ server/models/devicesModel.js
@@ -1,22 +1,18 @@
-const { connection } = require("../connection");
+const { connection } = require('../connection');
 
 exports.getDevices = ({ user_id }) => {
-  return connection("devices")
-    .where({ user_id })
-    .returning("*")
-    .then(returnedDevices => {
-      if (returnedDevices.length < 1)
-        return Promise.reject({ status: 404, msg: "No devices found" });
-      else return returnedDevices;
-    });
+  return connection
+    .select('*')
+    .from('devices')
+    .where({ user_id });
 };
 
 exports.postDevice = ({ user_id, push_key }) => {
   const device = { user_id, push_key };
   return connection
     .insert(device)
-    .into("devices")
-    .returning("*")
+    .into('devices')
+    .returning('*')
     .then(([addedDevice]) => {
       return addedDevice;
     });
