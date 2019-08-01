2c5d355ca64b6250400be277ffa507288e333707
Alice-Claire <aliceclaireh@hotmail.co.uk>
2019-08-01 15:58:34 +0100

    add amazon_id to devices table and test data

--- db/migrations/20190729104927_create_table_devices.js
+++ db/migrations/20190729104927_create_table_devices.js
@@ -1,14 +1,15 @@
 exports.up = function(knex) {
-  return knex.schema.createTable("devices", devicesTable => {
-    devicesTable.increments("id").primary();
+  return knex.schema.createTable('devices', devicesTable => {
+    devicesTable.increments('id').primary();
     devicesTable
-      .integer("user_id")
-      .references("users.id")
+      .integer('user_id')
+      .references('users.id')
       .notNullable();
-    devicesTable.string("push_key").notNullable();
+    devicesTable.string('push_key');
+    devicesTable.string('amazon_id');
   });
 };
 
 exports.down = function(knex) {
-  return knex.schema.dropTable("devices");
+  return knex.schema.dropTable('devices');
 };
--- db/test_data/devices.js
+++ db/test_data/devices.js
@@ -1,18 +1,26 @@
 module.exports = [
   {
     user_id: 3,
-    push_key: "83e83uish"
+    push_key: '83e83uish'
   },
   {
     user_id: 1,
-    push_key: "848sdsgdg"
+    push_key: '848sdsgdg'
   },
   {
     user_id: 2,
-    push_key: "83ffjfjf"
+    push_key: '83ffjfjf'
   },
   {
     user_id: 1,
-    push_key: "858djdhd"
+    amazon_id: 'a1234'
+  },
+  {
+    user_id: 2,
+    amazon_id: 'a4321'
+  },
+  {
+    user_id: 1,
+    push_key: '858djdhd'
   }
 ];
