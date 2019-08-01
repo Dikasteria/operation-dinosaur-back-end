cd87548eaa67e705ea8db1d19b64e1c3e3cb4f73
Alice-Claire <aliceclaireh@hotmail.co.uk>
2019-08-01 11:25:38 +0100

    add endpoint for endpoints.json

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,3 +1,5 @@
+const endpointControllers = require('./endpointsController');
+const codeControllers = require('./codesController');
 const userControllers = require('./usersController');
 const deviceControllers = require('./devicesController');
 const medsControllers = require('./medsController');
@@ -5,6 +7,8 @@ const eventControllers = require('./eventsController');
 const quizControllers = require('./quizController');
 
 module.exports = {
+  ...endpointControllers,
+  ...codeControllers,
   ...userControllers,
   ...deviceControllers,
   ...medsControllers,
