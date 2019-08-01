fecdfa6fa4261f4b332d114c4528720f575d4f90
Alice-Claire <aliceclaireh@hotmail.co.uk>
2019-08-01 11:54:05 +0100

    add codes controller - console.log only

---
+++ server/controllers/codesController.js
@@ -0,0 +1,9 @@
+const {} = require('../models/');
+
+exports.fetchCode = (req, res, next) => {
+  //getCode - generates new random code
+  //        - checks that it doesn't already exist,
+  //        - adds code to a new row in
+  //
+  console.log('fetching a code...');
+};
