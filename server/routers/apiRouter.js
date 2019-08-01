932450511e701de37bf09a088eff11b365d071da
Alice-Claire <aliceclaireh@hotmail.co.uk>
2019-08-01 12:35:02 +0100

    add codesRouter to apiRouter

--- server/routers/apiRouter.js
+++ server/routers/apiRouter.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const apiRouter = express.Router();
 const { fetchEndpoints } = require('../controllers/');
-// const { codesRouter } = require('./codesRouter');
+const { codesRouter } = require('./codesRouter');
 const { usersRouter } = require('./usersRouter');
 const { devicesRouter } = require('./devicesRouter');
 const { medsRouter } = require('./medsRouter');
@@ -9,7 +9,7 @@ const { eventsRouter } = require('./eventsRouter');
 const { quizRouter } = require('./quizRouter');
 
 apiRouter.route('/').get(fetchEndpoints);
-// apiRouter.use('/codes', codesRouter);
+apiRouter.use('/codes', codesRouter);
 apiRouter.use('/users', usersRouter);
 apiRouter.use('/devices', devicesRouter);
 apiRouter.use('/meds', medsRouter);
