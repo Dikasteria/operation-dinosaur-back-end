8e99e1f67c55c74da90861bcfe42de8a18dbd40e
Dikasteria <rjhey@hotmail.com>
2019-08-05 14:50:14 +0100

    amends data milliseconds

--- db/dev_data/events.js
+++ db/dev_data/events.js
@@ -1,9 +1,23 @@
 module.exports = [
   {
     user_id: 1,
-    time: 1564412400000,
-    description: "fell over at 3pm"
+    time: 1564992000,
+    description: "fell over at 7:30am"
   },
+  {
+    user_id: 1,
+    time: 1565002980,
+    description: "froze at 11:15am"
+  },
+  { user_id: 1, time: 1565088300000, description: "froze at 10:30am" },
+  { user_id: 1, time: 1565133000000, description: "fell over at 11:00pm" },
+  { user_id: 1, time: 1565180040000, description: "fell over at 12:00pm" },
+  { user_id: 1, time: 1565191380000, description: "froze at 3:15pm" },
+  { user_id: 1, time: 1565288640000, description: "fell over at 6:00pm" },
+  { user_id: 1, time: 1565262720000, description: "froze at 11:05pm" },
+  { user_id: 1, time: 1565360040000, description: "fell over at 2:10pm" },
+  { user_id: 1, time: 1565456100000, description: "froze at 4:50pm" },
+  { user_id: 1, time: 1565555520000, description: "froze at 8:30pm" },
   {
     user_id: 2,
     time: 1564412400000,
