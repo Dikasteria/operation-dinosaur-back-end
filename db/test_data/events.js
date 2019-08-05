d994c88706740e8b315acb4b3220ff707c72a70b
Dikasteria <rjhey@hotmail.com>
2019-08-05 12:42:40 +0100

    populates one week of data for one user

--- db/test_data/events.js
+++ db/test_data/events.js
@@ -1,9 +1,23 @@
 module.exports = [
   {
     user_id: 1,
-    time: 1564412400000,
-    description: "fell over at 3pm"
+    time: 1564992000,
+    description: "fell over at 7:30am"
   },
+  {
+    user_id: 1,
+    time: 1565002980,
+    description: "froze at 11:15am"
+  },
+  { user_id: 1, time: 1565088300, description: "froze at 10:30am" },
+  { user_id: 1, time: 1565133000, description: "fell over at 11:00pm" },
+  { user_id: 1, time: 1565180040, description: "fell over at 12:00pm" },
+  { user_id: 1, time: 1565191380, description: "froze at 3:15pm" },
+  { user_id: 1, time: 1565288640, description: "fell over at 6:00pm" },
+  { user_id: 1, time: 1565262720, description: "froze at 11:05pm" },
+  { user_id: 1, time: 1565360040, description: "fell over at 2:10pm" },
+  { user_id: 1, time: 1565456100, description: "froze at 4:50pm" },
+  { user_id: 1, time: 1565555520, description: "froze at 8:30pm" },
   {
     user_id: 2,
     time: 1564412400000,
--- db/test_data/meds.js
+++ db/test_data/meds.js
@@ -1,5 +1,61 @@
 module.exports = [
-  { user_id: 1, type: "ropinerol", due: 1564412400000 },
+  //8, 12, 4 & 8 for co-benaldopa, 8pm for sinemet, 8 for sertraline
+  { user_id: 1, type: "co-benaldopa", due: 1564992000 },
+  { user_id: 1, type: "co-benaldopa", due: 1565006400 },
+  { user_id: 1, type: "co-benaldopa", due: 1565020800 },
+  { user_id: 1, type: "co-benaldopa", due: 1565035200 },
+  { user_id: 1, type: "calcichew", due: 1565020800 },
+  { user_id: 1, type: "sinemet", due: 1565035200 },
+  { user_id: 1, type: "sertraline", due: 1565035200 },
+
+  { user_id: 1, type: "co-benaldopa", due: 1565078400 },
+  { user_id: 1, type: "co-benaldopa", due: 1565092800 },
+  { user_id: 1, type: "co-benaldopa", due: 1565107200 },
+  { user_id: 1, type: "co-benaldopa", due: 1565121600 },
+  { user_id: 1, type: "calcichew", due: 1565020800 },
+  { user_id: 1, type: "sinemet", due: 1565121600 },
+  { user_id: 1, type: "sertraline", due: 1565121600 },
+
+  { user_id: 1, type: "co-benaldopa", due: 1565164800 },
+  { user_id: 1, type: "co-benaldopa", due: 1565179200 },
+  { user_id: 1, type: "co-benaldopa", due: 1565193600 },
+  { user_id: 1, type: "co-benaldopa", due: 1565208000 },
+  { user_id: 1, type: "calcichew", due: 1565193600 },
+  { user_id: 1, type: "sinemet", due: 1565208000 },
+  { user_id: 1, type: "sertraline", due: 1565208000 },
+
+  { user_id: 1, type: "co-benaldopa", due: 1565251200 },
+  { user_id: 1, type: "co-benaldopa", due: 1565265600 },
+  { user_id: 1, type: "co-benaldopa", due: 1565280000 },
+  { user_id: 1, type: "co-benaldopa", due: 1565294400 },
+  { user_id: 1, type: "calcichew", due: 1565280000 },
+  { user_id: 1, type: "sinemet", due: 1565294400 },
+  { user_id: 1, type: "sertraline", due: 1565294400 },
+
+  { user_id: 1, type: "co-benaldopa", due: 1565337600 },
+  { user_id: 1, type: "co-benaldopa", due: 1565352000 },
+  { user_id: 1, type: "co-benaldopa", due: 1565366400 },
+  { user_id: 1, type: "co-benaldopa", due: 1565380800 },
+  { user_id: 1, type: "calcichew", due: 1565366400 },
+  { user_id: 1, type: "sinemet", due: 1565380800 },
+  { user_id: 1, type: "sertraline", due: 1565380800 },
+
+  { user_id: 1, type: "co-benaldopa", due: 1565424000 },
+  { user_id: 1, type: "co-benaldopa", due: 1565438400 },
+  { user_id: 1, type: "co-benaldopa", due: 1565452800 },
+  { user_id: 1, type: "co-benaldopa", due: 1565467200 },
+  { user_id: 1, type: "calcichew", due: 1565452800 },
+  { user_id: 1, type: "sinemet", due: 1565467200 },
+  { user_id: 1, type: "sertraline", due: 1565467200 },
+
+  { user_id: 1, type: "co-benaldopa", due: 1565510400 },
+  { user_id: 1, type: "co-benaldopa", due: 1565524800 },
+  { user_id: 1, type: "co-benaldopa", due: 1565539200 },
+  { user_id: 1, type: "co-benaldopa", due: 1565553600 },
+  { user_id: 1, type: "calcichew", due: 1565539200 },
+  { user_id: 1, type: "sinemet", due: 1565553600 },
+  { user_id: 1, type: "sertraline", due: 1565553600 },
+
   { user_id: 2, type: "paracetamol", due: 1564412400000 },
   { user_id: 3, type: "ibuprofen", due: 1564412400000 }
 ];
